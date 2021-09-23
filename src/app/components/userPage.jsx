167009a5c89bcda140d5a1ef143d2c008943421a
sergeevev <sergeevevit@gmail.com>
2021-09-24 02:06:06 +0500

    fix link "all users"

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -2,7 +2,7 @@ import api from "../api";
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import QualitiesList from "./qualitiesList";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const UserPage = ({ id }) => {
     const [user, setUser] = useState();
@@ -10,11 +10,6 @@ const UserPage = ({ id }) => {
     useEffect(() => {
         api.users.getById(id).then((user) => setUser(user));
     }, []);
-
-    const history = useHistory();
-    const handleAllUsert = () => {
-        history.push("/users");
-    };
 
     return (
         <>
@@ -25,13 +20,8 @@ const UserPage = ({ id }) => {
                     <QualitiesList qualities={user.qualities} />
                     <h4>completedMeetings: {user.completedMeetings}</h4>
                     <h3>Rate: {user.rate}</h3>
-                    <button
-                        className="btn btn-primary"
-                        onClick={() => {
-                            handleAllUsert();
-                        }}
-                    >
-                        Все пользователи
+                    <button>
+                        <Link to="/users">Все пользователи</Link>
                     </button>
                 </>
             )}
