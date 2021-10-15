6f0d4ebd71d3f7746b3951b1aba2ba3b6aa68708
sergeevev <sergeevevit@gmail.com>
2021-10-16 03:03:16 +0500

    forms part 1 lesson 8

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import TextField from "../components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
 
     const handleChange = ({ target }) => {
         setData((prevState) => ({
@@ -11,8 +14,27 @@ const Login = () => {
         }));
     };
 
+    const validatorConfig = {
+        email: { isRequired: { message: "email обязательная для заполнение" } },
+        password: {
+            isRequired: { message: "password обязателен для заполнение" }
+        }
+    };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
         console.log(data);
     };
 
@@ -23,6 +45,7 @@ const Login = () => {
                 name="email"
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 label="password"
@@ -30,6 +53,7 @@ const Login = () => {
                 name="password"
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
             <button type="submit">Submit</button>
         </form>
