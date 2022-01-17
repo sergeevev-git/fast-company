eb74440601d351195056910a594fb38d6ed76acd
sergeevev <sergeevevit@gmail.com>
2022-01-17 22:23:40 +0500

    fix crash without user-image

--- src/app/components/ui/avatar.jsx
+++ src/app/components/ui/avatar.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Avatar = ({ width, user }) => {
+    if (user.image) {
+        return (
+            <img
+                src={user.image}
+                className="rounded-circle shadow-1-strong me-3"
+                alt="avatar"
+                width={width}
+                height={width === "65" ? "65" : ""}
+            />
+        );
+    }
     return (
-        <img
-            src={user.image}
-            className="rounded-circle shadow-1-strong me-3"
-            alt="avatar"
-            width={width}
-            height={width === "65" ? "65" : ""}
-        />
+        <>
+            <i className="bi bi-image"></i>
+        </>
     );
 };
 
